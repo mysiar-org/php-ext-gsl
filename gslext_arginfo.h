bd3091779800ee99ea150b247dc794678025cc2d
Piotr Synowiec <mysiar@users.noreply.github.com>
2022-10-24 18:13:56 +0200

    GSL elementary functions (#2)
    * dev: broken stubs not generated
    
    * dev: GSL_log1p
    
    * dev: GSL_expm1
    
    * dev: GSL_hypot GSL_hypot3 GSL_acosh GSL_asinh GSL_atanh
    
    * dev: GSL_ldexp
    
    * dev: GSL_frexp

--- gslext_arginfo.h
+++ gslext_arginfo.h
@@ -1,20 +1,60 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: efdd79c2c8ccff694699c86fdd6248a13839c744 */
+ * Stub hash: 27eebf481840b7fb2626354a9f07103c670a89d3 */
 
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_test1, 0, 0, IS_VOID, 0)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_GSL_log1p, 0, 1, IS_DOUBLE, 0)
+	ZEND_ARG_TYPE_INFO(0, x, IS_DOUBLE, 0)
 ZEND_END_ARG_INFO()
 
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_test2, 0, 0, IS_STRING, 0)
-	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, str, IS_STRING, 0, "\"\"")
+#define arginfo_GSL_expm1 arginfo_GSL_log1p
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_GSL_hypot, 0, 2, IS_DOUBLE, 0)
+	ZEND_ARG_TYPE_INFO(0, x, IS_DOUBLE, 0)
+	ZEND_ARG_TYPE_INFO(0, y, IS_DOUBLE, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_GSL_hypot3, 0, 3, IS_DOUBLE, 0)
+	ZEND_ARG_TYPE_INFO(0, x, IS_DOUBLE, 0)
+	ZEND_ARG_TYPE_INFO(0, y, IS_DOUBLE, 0)
+	ZEND_ARG_TYPE_INFO(0, z, IS_DOUBLE, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_GSL_acosh arginfo_GSL_log1p
+
+#define arginfo_GSL_asinh arginfo_GSL_log1p
+
+#define arginfo_GSL_atanh arginfo_GSL_log1p
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_GSL_ldexp, 0, 2, IS_DOUBLE, 0)
+	ZEND_ARG_TYPE_INFO(0, x, IS_DOUBLE, 0)
+	ZEND_ARG_TYPE_INFO(0, e, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_GSL_frexp, 0, 2, IS_DOUBLE, 0)
+	ZEND_ARG_TYPE_INFO(0, x, IS_DOUBLE, 0)
+	ZEND_ARG_TYPE_INFO(1, e, IS_LONG, 0)
 ZEND_END_ARG_INFO()
 
 
-ZEND_FUNCTION(test1);
-ZEND_FUNCTION(test2);
+ZEND_FUNCTION(GSL_log1p);
+ZEND_FUNCTION(GSL_expm1);
+ZEND_FUNCTION(GSL_hypot);
+ZEND_FUNCTION(GSL_hypot3);
+ZEND_FUNCTION(GSL_acosh);
+ZEND_FUNCTION(GSL_asinh);
+ZEND_FUNCTION(GSL_atanh);
+ZEND_FUNCTION(GSL_ldexp);
+ZEND_FUNCTION(GSL_frexp);
 
 
 static const zend_function_entry ext_functions[] = {
-	ZEND_FE(test1, arginfo_test1)
-	ZEND_FE(test2, arginfo_test2)
+	ZEND_FE(GSL_log1p, arginfo_GSL_log1p)
+	ZEND_FE(GSL_expm1, arginfo_GSL_expm1)
+	ZEND_FE(GSL_hypot, arginfo_GSL_hypot)
+	ZEND_FE(GSL_hypot3, arginfo_GSL_hypot3)
+	ZEND_FE(GSL_acosh, arginfo_GSL_acosh)
+	ZEND_FE(GSL_asinh, arginfo_GSL_asinh)
+	ZEND_FE(GSL_atanh, arginfo_GSL_atanh)
+	ZEND_FE(GSL_ldexp, arginfo_GSL_ldexp)
+	ZEND_FE(GSL_frexp, arginfo_GSL_frexp)
 	ZEND_FE_END
 };
---
+++ inc/elementary.c
@@ -0,0 +1,176 @@
+#include "gsl/gsl_sys.h"
+
+PHP_FUNCTION(GSL_log1p)
+{
+    zval *p_x;
+    double x;
+    double rv;
+
+    ZEND_PARSE_PARAMETERS_START(1, 1)
+        Z_PARAM_ZVAL(p_x);
+    ZEND_PARSE_PARAMETERS_END();
+
+    convert_to_double(p_x);
+    x = Z_DVAL_P(p_x);
+    rv = gsl_log1p(x);
+
+    RETURN_DOUBLE(rv);
+}
+
+PHP_FUNCTION(GSL_expm1)
+{
+    zval *p_x;
+    double x;
+    double rv;
+
+    ZEND_PARSE_PARAMETERS_START(1, 1)
+        Z_PARAM_ZVAL(p_x);
+    ZEND_PARSE_PARAMETERS_END();
+
+    convert_to_double(p_x);
+    x = Z_DVAL_P(p_x);
+    rv = gsl_expm1(x);
+
+    RETURN_DOUBLE(rv);
+}
+
+PHP_FUNCTION(GSL_hypot)
+{
+    zval *p_x, *p_y;
+    double x, y;
+    double rv;
+
+    ZEND_PARSE_PARAMETERS_START(2, 2)
+        Z_PARAM_ZVAL(p_x);
+        Z_PARAM_ZVAL(p_y);
+    ZEND_PARSE_PARAMETERS_END();
+
+    convert_to_double(p_x);
+    convert_to_double(p_y);
+    x = Z_DVAL_P(p_x);
+    y = Z_DVAL_P(p_y);
+    rv = gsl_hypot(x, y);
+
+    RETURN_DOUBLE(rv);
+}
+
+PHP_FUNCTION(GSL_hypot3)
+{
+    zval *p_x, *p_y, *p_z;
+    double x, y, z;
+    double rv;
+
+    ZEND_PARSE_PARAMETERS_START(3, 3)
+        Z_PARAM_ZVAL(p_x);
+        Z_PARAM_ZVAL(p_y);
+        Z_PARAM_ZVAL(p_z);
+    ZEND_PARSE_PARAMETERS_END();
+
+    convert_to_double(p_x);
+    convert_to_double(p_y);
+    convert_to_double(p_z);
+    x = Z_DVAL_P(p_x);
+    y = Z_DVAL_P(p_y);
+    z = Z_DVAL_P(p_z);
+    rv = gsl_hypot3(x, y, z);
+
+    RETURN_DOUBLE(rv);
+}
+
+PHP_FUNCTION(GSL_acosh)
+{
+    zval *p_x;
+    double x;
+    double rv;
+
+    ZEND_PARSE_PARAMETERS_START(1, 1)
+    Z_PARAM_ZVAL(p_x);
+    ZEND_PARSE_PARAMETERS_END();
+
+    convert_to_double(p_x);
+    x = Z_DVAL_P(p_x);
+    rv = gsl_acosh(x);
+
+    RETURN_DOUBLE(rv);
+}
+
+PHP_FUNCTION(GSL_asinh)
+{
+    zval *p_x;
+    double x;
+    double rv;
+
+    ZEND_PARSE_PARAMETERS_START(1, 1)
+    Z_PARAM_ZVAL(p_x);
+    ZEND_PARSE_PARAMETERS_END();
+
+    convert_to_double(p_x);
+    x = Z_DVAL_P(p_x);
+    rv = gsl_asinh(x);
+
+    RETURN_DOUBLE(rv);
+}
+
+PHP_FUNCTION(GSL_atanh)
+{
+    zval *p_x;
+    double x;
+    double rv;
+
+    ZEND_PARSE_PARAMETERS_START(1, 1)
+    Z_PARAM_ZVAL(p_x);
+    ZEND_PARSE_PARAMETERS_END();
+
+    convert_to_double(p_x);
+    x = Z_DVAL_P(p_x);
+    rv = gsl_atanh(x);
+
+    RETURN_DOUBLE(rv);
+}
+
+PHP_FUNCTION(GSL_ldexp)
+{
+    zval *p_x, *p_e;
+    double x;
+    int e;
+    double rv;
+
+    ZEND_PARSE_PARAMETERS_START(2, 2)
+    Z_PARAM_ZVAL(p_x);
+    Z_PARAM_ZVAL(p_e);
+    ZEND_PARSE_PARAMETERS_END();
+
+    convert_to_double(p_x);
+    convert_to_long(p_e);
+    x = Z_DVAL_P(p_x);
+    e = (int) Z_LVAL_P(p_e);
+    rv = gsl_ldexp(x, e);
+
+    RETURN_DOUBLE(rv);
+}
+
+PHP_FUNCTION(GSL_frexp)
+{
+    zval *p_x, *p_e;
+    double x;
+    int e;
+    double rv;
+
+    ZEND_PARSE_PARAMETERS_START(2, 2)
+        Z_PARAM_ZVAL(p_x);
+        Z_PARAM_ZVAL(p_e);
+    ZEND_PARSE_PARAMETERS_END();
+
+    ZVAL_DEREF(p_e);
+
+    convert_to_double(p_x);
+    convert_to_long(p_e);
+    x = Z_DVAL_P(p_x);
+    e = (int) Z_LVAL_P(p_e);
+    rv = gsl_frexp(x, &e);
+
+    Z_LVAL_P(p_e) = e;
+
+    RETURN_DOUBLE(rv);
+}
+
